86a54f7e96219e4ddf6d36a96d808bac9ab7614f
Claudio Maesi <claudiomaesi@gmail.com>
2023-11-10 15:08:32 -0300

    Teste de comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,31 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
+    test('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    test('Deve adiconar 2 comentários na lista', () => {
+        render(<PostComment />)
+
+        // Adiciona comentário 1
+        fireEvent.change(screen.getByTestId('textArea-comentario'), {
+            target:
+            { value: 'Muito legal' }})
+        fireEvent.click(screen.getByTestId('btn-adicionar-comentario'))        
+        
+        // adiciona comentário 2
+        fireEvent.change(screen.getByTestId('textArea-comentario'),
+            {target:
+            { value: 'Sensacional' }})
+        fireEvent.click(screen.getByTestId('btn-adicionar-comentario'))
+        
+        // Verifica se há 2 itens na lista
+        expect(screen.getAllByTestId('li-comentario')).toHaveLength(2)
+    })
+
+
+
 });
